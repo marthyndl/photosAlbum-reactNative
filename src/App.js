25c9cb100f329aa41658df6b21b52ceb1405a974
martindlopez <martin.lopez@wenance.com>
2019-05-20 04:14:52 -0300

    Async storage configuration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import thunk from 'redux-thunk';
+import { images } from './reducers';
 
-const instructions = Platform.select({
-  ios: 'https://www.linkedin.com/in/martín-daniel-lopez-45ba30aa',
-  android: 'https://www.linkedin.com/in/martín-daniel-lopez-45ba30aa'
+const imagesPersistConfig = {
+  key: 'images',
+  storage: AsyncStorage
+};
+
+const reducer = combineReducers({
+  images: persistReducer(imagesPersistConfig, images)
 });
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+const persistor = persistStore(store);
 
 export default class App extends Component {
   constructor(props) {
@@ -12,18 +27,6 @@ export default class App extends Component {
     this.state = {
       data: ''
     };
-  }
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/photos')
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState({
-          data: responseJson
-        });
-      })
-      .catch(err => {
-        this.setState({ data: err });
-      });
   }
   render() {
     if (this.state.data) {
@@ -34,28 +37,11 @@ export default class App extends Component {
       });
     }
     return (
-      <View style={styles.container}>
-        <Text>{this.state.data ? res : 'Loading!!! ...'}</Text>
-      </View>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Text>{this.state.data ? res : 'Loading!!! ...'}</Text>
+        </PersistGate>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-});
